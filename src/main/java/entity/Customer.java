d2cc769a32489c7c62d2f454c9d39211a6319bf3
Ujitha <ujithamigara@gmail.com>
2024-01-07 17:17:21 +0530

    add hibernate  and create customer entity

---
+++ src/main/java/entity/Customer.java
@@ -0,0 +1,18 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    private String customerId;
+    private String fullName;
+    private String address;
+}
