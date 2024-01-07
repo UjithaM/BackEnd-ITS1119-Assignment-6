1916f1afdb0e2ddecabb034e8fba0c079fc26457
Ujitha <ujithamigara@gmail.com>
2024-01-07 11:32:16 +0530

    add Item and Order

---
+++ src/main/java/api/Order.java
@@ -0,0 +1,9 @@
+package api;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "customer",urlPatterns = "/customer")
+public class Order extends HttpServlet {
+
+}
