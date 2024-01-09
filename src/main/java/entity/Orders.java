9cc22882597ba0f5f615fb98d68882890b054314
Ujitha <ujithamigara@gmail.com>
2024-01-09 23:22:07 +0530

    change fetch to Eager

--- src/main/java/entity/Orders.java
+++ src/main/java/entity/Orders.java
@@ -1,9 +1,12 @@
 package entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -23,6 +26,7 @@ public class Orders {
     private Double cash;
     private Double subTotal;
 
-    @OneToMany(mappedBy = "orders", cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.EAGER,mappedBy = "orders", cascade = CascadeType.ALL) @Fetch(FetchMode.JOIN)
+    @JsonManagedReference
     private List<OrderItem> orderItems;
 }
